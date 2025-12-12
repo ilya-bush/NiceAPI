6f6d38941da60bcc891a3ef6d6bbfd8e19d28670
FarmMan <kusilla16@gmail.com>
2025-12-12 19:08:25 +0200

    Added 19 messages to ChatUtil.java

--- src/main/java/learn/code/niceapi/NiceAPI.java
+++ src/main/java/learn/code/niceapi/NiceAPI.java
@@ -4,17 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 import static learn.code.niceapi.utils.LogsUtil.log;
 
 public final class NiceAPI extends JavaPlugin {
-
     @Override
     public void onEnable() {
         learn.code.niceapi.utils.LogsUtil.init(this.getLogger(), this.getName());
 
-        log("[NiceAPI] Enabled!");
+        log("Enabled!");
         log("Version: " + this.getDescription().getVersion());
     }
 
     @Override
     public void onDisable() {
-        log("Plugin has been disabled!");
+        log("Disabled!");
     }
 }
--- src/main/java/learn/code/niceapi/utils/ChatUtil.java
+++ src/main/java/learn/code/niceapi/utils/ChatUtil.java
@@ -2,6 +2,7 @@ package learn.code.niceapi.utils;
 
 import learn.code.niceapi.NiceAPI;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 
 public class ChatUtil {
     private final NiceAPI pl;
@@ -13,12 +14,48 @@ public class ChatUtil {
     // Color Parser
     private String getMessage(String path) {
         String raw = pl.getConfig().getString(path);
-        if (raw == null) return "";
+        if (raw == null) return ChatColor.RED + "Missing " + path;
         return ChatColor.translateAlternateColorCodes('&', raw);
     }
 
     // Main Messages
-    public String playerNotOnlineMessage() { return getMessage("messages.player-not-online"); }
-    public String noPermissionMessage() { return getMessage("messages.no-permission"); }
-    public String notPlayerMessage() { return getMessage("messages.not-player"); }
+    public String givedBowMessage() { return getMessage("messages.gived-bow"); }
+    public String giveOnJoinMessage() { return getMessage("messages.give-on-join"); }
+    public String bowNameMessage() { return getMessage("messages.bow"); }
+    public String bowDescriptionMessage() { return getMessage("messages.bow-description"); }
+
+    // Feed Command Messages (/feed)
+    public String fedMessage() { return getMessage("messages.fed"); }
+
+    // Fly Command Messages (/fly)
+    public String flyOnMessage() { return getMessage("messages.fly-on"); }
+    public String flyOffMessage() { return getMessage("messages.fly-off"); }
+
+    // God Command Messages (/god)
+    public String godOnMessage() { return getMessage("god-on"); }
+    public String godOffMessage() { return getMessage("god-off"); }
+
+    // Heal Command Messages (/heal)
+    public String healedMessage() { return getMessage("messages.healed"); }
+
+    // Invisible Command Messages (/invisible)
+    public String invisibleOnMessage() { return getMessage("messages.invisible-on"); }
+    public String invisibleOffMessage() { return getMessage("messages.invisible-off"); }
+
+    // Clear Inventory Command Messages (/clearinventory)
+    public String clearedInventoryMessage() { return getMessage("messages.cleared-inventory"); }
+
+    // On Events Messages
+    public String shearSheepMessage() { return getMessage("messages.shear-sheep"); }
+    public String shearMushroomCowMessage() { return getMessage("messages.shear-mushroom-cow"); }
+
+    // Spawn And SetSpawn Commands Messages (/spawn, /setspawn)
+    public String spawnSetMessage() { return getMessage("messages.spawn-set"); }
+    public String spawnNotSetMessage() { return getMessage("messages.spawn-not-set"); }
+    public String teleportedToSpawnMessage() { return getMessage("messages.teleported-to-spawn"); }
+    public Location getSpawnLocationMessage() { return pl.getConfig().getLocation("spawn"); }
+    public String getWorldName() { return pl.getConfig().getString("spawn.world"); }
+    public double getX() { return pl.getConfig().getDouble("spawn.x"); }
+    public double getY() { return pl.getConfig().getDouble("spawn.y"); }
+    public double getZ() { return pl.getConfig().getDouble("spawn.z"); }
 }
