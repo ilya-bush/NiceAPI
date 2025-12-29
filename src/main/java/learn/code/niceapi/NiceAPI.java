1665b84c25ec155bd3286db68ad939430651e443
FarmMan <kusilla16@gmail.com>
2025-12-29 11:59:19 +0200

    1.8. deleted test functions and classes.

--- src/main/java/learn/code/niceapi/NiceAPI.java
+++ src/main/java/learn/code/niceapi/NiceAPI.java
@@ -1,8 +1,5 @@
 package learn.code.niceapi;
 
-import learn.code.niceapi.test.MenuListener;
-import learn.code.niceapi.test.Menus;
-import learn.code.niceapi.test.TestCommand;
 import learn.code.niceapi.utils.Logs;
 import static learn.code.niceapi.utils.Logs.log;
 
@@ -13,7 +10,6 @@ import org.slf4j.LoggerFactory;
 public final class NiceAPI extends JavaPlugin {
 
     private static final Logger log = LoggerFactory.getLogger(NiceAPI.class);
-    private Menus menusMenu;
 
     public NiceAPI() {}
 
@@ -22,11 +18,6 @@ public final class NiceAPI extends JavaPlugin {
 
         // Logs util initialization
         Logs.init(getComponentLogger(), getName());
-
-        //from test
-        menusMenu = new Menus(this);
-        getCommand("test").setExecutor(new TestCommand(this));
-        getServer().getPluginManager().registerEvents(new MenuListener(this), this);
 
         log("<green>NiceAPI Enabled!");
         log("<green>[NiceAPI] Version: " + getDescription().getVersion());
@@ -38,9 +29,5 @@ public final class NiceAPI extends JavaPlugin {
 
         log("NiceAPI Disabled!");
 
-    }
-
-    public Menus getMenusManager() {
-        return menusMenu;
     }
 }
